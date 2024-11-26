0ac8aa300f74ed8338bbd2c757b5f0edf957ce3f
Lucas-Armand <oliveira.armand@poli.ufrj.br>
2024-11-26 20:18:17 -0300

    Update utils_erase_elastic.py

--- tests/utils_erase_elastic.py
+++ tests/utils_erase_elastic.py
@@ -5,7 +5,13 @@ ELASTIC_HOST = "http://elasticsearch:9200"
 
 es = Elasticsearch(ELASTIC_HOST)
 
-response = es.delete_by_query(index="_all", body={"query": {"match_all": {}}}, ignore=[400, 404])
+response = es.delete_by_query(index="bus", body={"query": {"match_all": {}}}, ignore=[400, 404])
+print("Resposta do Elasticsearch:", response)
+
+response = es.delete_by_query(index="van", body={"query": {"match_all": {}}}, ignore=[400, 404])
+print("Resposta do Elasticsearch:", response)
+
+response = es.delete_by_query(index="stop", body={"query": {"match_all": {}}}, ignore=[400, 404])
 print("Resposta do Elasticsearch:", response)
 
 es.transport.close()

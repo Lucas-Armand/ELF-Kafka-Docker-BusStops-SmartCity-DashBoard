8dafb3a9f47a2c2a2bd85911379c4fb4826a5858
Lucas-Armand <oliveira.armand@poli.ufrj.br>
2024-11-26 20:02:44 -0300

    Update process.py

--- process_raw_to_cleaned_vans/process.py
+++ process_raw_to_cleaned_vans/process.py
@@ -36,7 +36,7 @@ def main(producer, consumer):
             print(f"Error processing: {e}")
 
 
-# Configurar o consumidor Kafka
+# Configure consumer
 consumer = KafkaConsumer(
     RAW_TOPIC,
     bootstrap_servers=KAFKA_BROKER,
@@ -46,7 +46,7 @@ consumer = KafkaConsumer(
     group_id='van_processor_group'
 )
 
-# Configurar o produtor Kafka
+# Configure producer
 producer = KafkaProducer(
     bootstrap_servers=KAFKA_BROKER,
     value_serializer=lambda v: json.dumps(v).encode('utf-8')
